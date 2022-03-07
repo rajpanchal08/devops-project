1a2987f171419173ed4e432d3dc691d54307a3ee
First Last <rajpanchal181gm@ip-172-31-81-104.ec2.internal>
2022-03-07 15:43:53 +0000

    Project Code

---
+++ ec2.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+ 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+ 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+ 
+ 
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "First-ec2" {
+  ami            = data.aws_ami.ubuntu.id
+  instance_type  = "t2.micro"
+  key_name  = "project"
+  tags  = {
+    Name = "First-ec2"
+   }
+}
+resource "aws_instance" "Second-ec2" {
+  ami            = data.aws_ami.ubuntu.id
+  instance_type  = "t2.micro"
+  key_name  = "project"
+  tags  = {
+    Name = "Second-ec2"
+   }
+}
+
